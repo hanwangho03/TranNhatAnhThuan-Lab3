28052dda835d3be00f020b2369f0a12f57834a59
The Bao <anhthuan06042003@gmail.com>
2024-06-03 00:35:55 +0700

    Initial commit

---
+++ src/main/java/Lab3/TranNhatAnhThuan_Lab3/BookController.java
@@ -0,0 +1,55 @@
+package Lab3.TranNhatAnhThuan_Lab3;
+
+import Lab3.TranNhatAnhThuan_Lab3.services.BookService;
+import entities.Book;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@Controller
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+    @GetMapping
+    public String showAllBooks(@NotNull Model model) {
+        model.addAttribute("books", bookService.getAllBooks());
+        return "book/list";
+    }
+    @GetMapping("/add")
+    public String addBookForm(@NotNull Model model) {
+        model.addAttribute("book", new Book());
+        return "book/add";
+    }
+    @PostMapping("/add")
+    public String addBook(@ModelAttribute("book") Book book) {
+        if(bookService.getBookById(book.getId()).isEmpty())
+            bookService.addBook(book);
+        return "redirect:/books";
+    }
+    @GetMapping("/index")
+    public String showHomePage() {
+        return "book/index"; // Đây là tên của file index.html trong thư mục templates/home
+    }
+    @GetMapping("/edit/{id}")
+    public String editBookForm(@NotNull Model model, @PathVariable long id)
+    {
+        var book = bookService.getBookById(id).orElse(null);
+        model.addAttribute("book", book != null ? book : new Book());
+        return "book/edit";
+    }
+    @PostMapping("/edit")
+    public String editBook(@ModelAttribute("book") Book book) {
+        bookService.updateBook(book);
+        return "redirect:/books";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable long id) {
+        if (bookService.getBookById(id).isPresent())
+            bookService.deleteBookById(id);
+        return "redirect:/books";
+    }
+}
---
+++ src/main/java/Lab3/TranNhatAnhThuan_Lab3/TranNhatAnhThuanLab3Application.java
@@ -0,0 +1,13 @@
+package Lab3.TranNhatAnhThuan_Lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranNhatAnhThuanLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranNhatAnhThuanLab3Application.class, args);
+	}
+
+}
